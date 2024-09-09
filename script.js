a2c6907162873e60712b4ef8db0220007b64fad5
Daley Mottley <daley.mottley@hotmail.com>
2024-09-08 22:17:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,62 +1,88 @@
 "use strict";
 
-var accessToken = '3796899bd37c423bad3a21a25277bce0';
-var baseUrl = 'https://api.api.ai/api/query?v=2015091001';
-var sessionId = '20150910';
-var loader = "<span class='loader'><span class='loader__dot'></span><span class='loader__dot'></span><span class='loader__dot'></span></span>";
-var errorMessage = 'My apologies, I\'m not avail at the moment, however, feel free to call our support team directly 0123456789.';
-var urlPattern = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-var $document = document;
-var $chatbot = $document.querySelector('.chatbot');
-var $chatbotMessageWindow = $document.querySelector('.chatbot__message-window');
-var $chatbotHeader = $document.querySelector('.chatbot__header');
-var $chatbotMessages = $document.querySelector('.chatbot__messages');
-var $chatbotInput = $document.querySelector('.chatbot__input');
-var $chatbotSubmit = $document.querySelector('.chatbot__submit');
-var botLoadingDelay = 1000;
-var botReplyDelay = 2000;
-document.addEventListener('keypress', function (event) {
+// Load environment variables
+require('dotenv').config();
+
+const accessToken = process.env.ACCESS_TOKEN;
+const baseUrl = 'https://api.api.ai/api/query?v=2015091001';
+const sessionId = '20150910';
+const loader = "<span class='loader'><span class='loader__dot'></span><span class='loader__dot'></span><span class='loader__dot'></span></span>";
+const errorMessage = 'My apologies, I\'m not available at the moment, however, feel free to call our support team directly at 0123456789.';
+const urlPattern = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+const $document = document;
+const $chatbot = $document.querySelector('.chatbot');
+const $chatbotMessageWindow = $document.querySelector('.chatbot__message-window');
+const $chatbotHeader = $document.querySelector('.chatbot__header');
+const $chatbotMessages = $document.querySelector('.chatbot__messages');
+const $chatbotInput = $document.querySelector('.chatbot__input');
+const $chatbotSubmit = $document.querySelector('.chatbot__submit');
+const botLoadingDelay = 1000;
+const botReplyDelay = 2000;
+
+document.addEventListener('keypress', (event) => {
   if (event.which == 13) validateMessage();
 }, false);
-$chatbotHeader.addEventListener('click', function () {
+
+$chatbotHeader.addEventListener('click', () => {
   toggle($chatbot, 'chatbot--closed');
   $chatbotInput.focus();
 }, false);
-$chatbotSubmit.addEventListener('click', function () {
+
+$chatbotSubmit.addEventListener('click', () => {
   validateMessage();
 }, false);
-var toggle = function toggle(element, klass) {
-  var classes = element.className.match(/\S+/g) || [],
-    index = classes.indexOf(klass);
+
+const toggle = (element, klass) => {
+  const classes = element.className.match(/\S+/g) || [];
+  const index = classes.indexOf(klass);
   index >= 0 ? classes.splice(index, 1) : classes.push(klass);
   element.className = classes.join(' ');
 };
-var userMessage = function userMessage(content) {
-  $chatbotMessages.innerHTML += "<li class='is-user animation'>\n      <p class='chatbot__message'>\n        ".concat(content, "\n      </p>\n      <span class='chatbot__arrow chatbot__arrow--right'></span>\n    </li>");
+
+const userMessage = (content) => {
+  $chatbotMessages.innerHTML += `<li class='is-user animation'>
+      <p class='chatbot__message'>
+        ${content}
+      </p>
+      <span class='chatbot__arrow chatbot__arrow--right'></span>
+    </li>`;
 };
-var aiMessage = function aiMessage(content) {
-  var isLoading = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
-  var delay = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 0;
-  setTimeout(function () {
+
+const aiMessage = (content, isLoading = false, delay = 0) => {
+  setTimeout(() => {
     removeLoader();
-    $chatbotMessages.innerHTML += "<li \n      class='is-ai animation' \n      id='".concat(isLoading ? "is-loading" : "", "'>\n        <div class=\"is-ai__profile-picture\">\n          <svg class=\"icon-avatar\" viewBox=\"0 0 32 32\">\n            <use xlink:href=\"#avatar\" />\n          </svg>\n        </div>\n        <span class='chatbot__arrow chatbot__arrow--left'></span>\n        <div class='chatbot__message'>").concat(content, "</div>\n      </li>");
+    $chatbotMessages.innerHTML += `<li 
+      class='is-ai animation' 
+      id='${isLoading ? "is-loading" : ""}'>
+        <div class="is-ai__profile-picture">
+          <svg class="icon-avatar" viewBox="0 0 32 32">
+            <use xlink:href="#avatar" />
+          </svg>
+        </div>
+        <span class='chatbot__arrow chatbot__arrow--left'></span>
+        <div class='chatbot__message'>${content}</div>
+      </li>`;
     scrollDown();
   }, delay);
 };
-var removeLoader = function removeLoader() {
-  var loadingElem = document.getElementById('is-loading');
+
+const removeLoader = () => {
+  const loadingElem = document.getElementById('is-loading');
   if (loadingElem) $chatbotMessages.removeChild(loadingElem);
 };
-var escapeScript = function escapeScript(unsafe) {
-  var safeString = unsafe.replace(/</g, ' ').replace(/>/g, ' ').replace(/&/g, ' ').replace(/"/g, ' ').replace(/\\/, ' ').replace(/\s+/g, ' ');
+
+const escapeScript = (unsafe) => {
+  const safeString = unsafe.replace(/</g, ' ').replace(/>/g, ' ').replace(/&/g, ' ').replace(/"/g, ' ').replace(/\\/, ' ').replace(/\s+/g, ' ');
   return safeString.trim();
 };
-var linkify = function linkify(inputText) {
+
+const linkify = (inputText) => {
   return inputText.replace(urlPattern, "<a href='$1' target='_blank'>$1</a>");
 };
-var validateMessage = function validateMessage() {
-  var text = $chatbotInput.value;
-  var safeText = text ? escapeScript(text) : '';
+
+const validateMessage = () => {
+  const text = $chatbotInput.value;
+  const safeText = text ? escapeScript(text) : '';
   if (safeText.length && safeText !== ' ') {
     resetInputField();
     userMessage(safeText);
@@ -65,47 +91,58 @@ var validateMessage = function validateMessage() {
   scrollDown();
   return;
 };
-var multiChoiceAnswer = function multiChoiceAnswer(text) {
-  var decodedText = text.replace(/zzz/g, "'");
+
+const multiChoiceAnswer = (text) => {
+  const decodedText = text.replace(/zzz/g, "'");
   userMessage(decodedText);
   send(decodedText);
   scrollDown();
   return;
 };
-var processResponse = function processResponse(val) {
+
+const processResponse = (val) => {
   if (val && val.fulfillment) {
-    var output = '';
-    var messagesLength = val.fulfillment.messages.length;
-    for (var i = 0; i < messagesLength; i++) {
-      var message = val.fulfillment.messages[i];
-      var type = message.type;
+    let output = '';
+    const messagesLength = val.fulfillment.messages.length;
+    for (let i = 0; i < messagesLength; i++) {
+      const message = val.fulfillment.messages[i];
+      const type = message.type;
       switch (type) {
         // 0 fulfillment is text
         case 0:
-          var parsedText = linkify(message.speech);
-          output += "<p>".concat(parsedText, "</p>");
+          const parsedText = linkify(message.speech);
+          output += `<p>${parsedText}</p>`;
           break;
 
         // 1 fulfillment is card
         case 1:
-          var imageUrl = message.imageUrl;
-          var imageTitle = message.title;
-          var imageSubtitle = message.subtitle;
-          var button = message.buttons[0];
+          const imageUrl = message.imageUrl;
+          const imageTitle = message.title;
+          const imageSubtitle = message.subtitle;
+          const button = message.buttons[0];
           if (!imageUrl && !button && !imageTitle && !imageSubtitle) break;
-          output += "\n            <a class='card' href='".concat(button.postback, "' target='_blank'>\n              <img src='").concat(imageUrl, "' alt='").concat(imageTitle, "' />\n            <div class='card-content'>\n              <h4 class='card-title'>").concat(imageTitle, "</h4>\n              <p class='card-title'>").concat(imageSubtitle, "</p>\n              <span class='card-button'>").concat(button.text, "</span>\n            </div>\n            </a>\n          ");
+          output += `
+            <a class='card' href='${button.postback}' target='_blank'>
+              <img src='${imageUrl}' alt='${imageTitle}' />
+            <div class='card-content'>
+              <h4 class='card-title'>${imageTitle}</h4>
+              <p class='card-title'>${imageSubtitle}</p>
+              <span class='card-button'>${button.text}</span>
+            </div>
+            </a>
+          `;
           break;
 
         // 2 fulfillment is a quick reply with multi-choice buttons
         case 2:
-          var title = message.title;
-          var replies = message.replies;
-          var repliesLength = replies.length;
-          output += "<p>".concat(title, "</p>");
-          for (var _i = 0; _i < repliesLength; _i++) {
-            var reply = replies[_i];
-            var encodedText = reply.replace(/'/g, 'zzz');
-            output += "<button onclick='multiChoiceAnswer(\"".concat(encodedText, "\")'>").concat(reply, "</button>");
+          const title = message.title;
+          const replies = message.replies;
+          const repliesLength = replies.length;
+          output += `<p>${title}</p>`;
+          for (let i = 0; i < repliesLength; i++) {
+            const reply = replies[i];
+            const encodedText = reply.replace(/'/g, 'zzz');
+            output += `<button onclick='multiChoiceAnswer("${encodedText}")'>${reply}</button>`;
           }
           break;
       }
@@ -114,45 +151,46 @@ var processResponse = function processResponse(val) {
     return output;
   }
   removeLoader();
-  return "<p>".concat(errorMessage, "</p>");
+  return `<p>${errorMessage}</p>`;
 };
-var setResponse = function setResponse(val) {
-  var delay = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
-  setTimeout(function () {
+
+const setResponse = (val, delay = 0) => {
+  setTimeout(() => {
     aiMessage(processResponse(val));
   }, delay);
 };
-var resetInputField = function resetInputField() {
+
+const resetInputField = () => {
   $chatbotInput.value = '';
 };
-var scrollDown = function scrollDown() {
-  var distanceToScroll = $chatbotMessageWindow.scrollHeight - ($chatbotMessages.lastChild.offsetHeight + 60);
+
+const scrollDown = () => {
+  const distanceToScroll = $chatbotMessageWindow.scrollHeight - ($chatbotMessages.lastChild.offsetHeight + 60);
   $chatbotMessageWindow.scrollTop = distanceToScroll;
   return false;
 };
-var send = function send() {
-  var text = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
-  fetch("".concat(baseUrl, "&query=").concat(text, "&lang=en&sessionId=").concat(sessionId), {
+
+const send = (text = '') => {
+  fetch(`${baseUrl}&query=${text}&lang=en&sessionId=${sessionId}`, {
     method: 'GET',
     dataType: 'json',
     headers: {
       Authorization: 'Bearer ' + accessToken,
       'Content-Type': 'application/json; charset=utf-8'
     }
-  }).then(function (response) {
-    return response.json();
-  }).then(function (res) {
-    if (res.status < 200 || res.status >= 300) {
-      var error = new Error(res.statusText);
-      throw error;
-    }
-    return res;
-  }).then(function (res) {
-    setResponse(res.result, botLoadingDelay + botReplyDelay);
-  })["catch"](function (error) {
-    setResponse(errorMessage, botLoadingDelay + botReplyDelay);
-    resetInputField();
-    console.log(error);
-  });
+  }).then(response => response.json())
+    .then(res => {
+      if (res.status < 200 || res.status >= 300) {
+        const error = new Error(res.statusText);
+        throw error;
+      }
+      return res;
+    }).then(res => {
+      setResponse(res.result, botLoadingDelay + botReplyDelay);
+    }).catch(error => {
+      setResponse(errorMessage, botLoadingDelay + botReplyDelay);
+      resetInputField();
+      console.log(error);
+    });
   aiMessage(loader, true, botLoadingDelay);
 };
