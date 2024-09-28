7e77b018bb9ee370de6387f068bf3afc4fface49
Daley Mottley <daley.mottley@hotmail.com>
2024-09-28 01:35:50 -0400

    Change model to gpt-4o-mini in ajax-handlers.php

--- includes/ajax-handlers.php
+++ includes/ajax-handlers.php
@@ -64,7 +64,7 @@ function vacw_get_bot_response() {
 
                 // Send the function response back to the assistant
                 $args['body'] = json_encode(array(
-                    'model'    => 'gpt-3.5-turbo-0613',
+                    'model'    => 'gpt-4o-mini',
                     'messages' => array(
                         array('role' => 'system', 'content' => $prompt['system']),
                         array('role' => 'user', 'content' => $prompt['user']),
